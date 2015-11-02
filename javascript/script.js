9ed5292398a0711d682c05a03c2cfa1794496f24
Harrison Riddell <hegriddell@gmail.com>
2015-11-01 19:48:06 -0500

    timer and score

--- javascript/script.js
+++ javascript/script.js
@@ -1,139 +1,141 @@
 var game = new Phaser.Game(1200, 720, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
-var upKey;
-var downKey;
-var leftKey;
-var rightKey;
 ///////////////////////////////////////////////////////
-//                    PRELOAD                        //
+//              PRELOAD - FUNCTION                   //
 ///////////////////////////////////////////////////////
-
-  function preload() {
-    game.load.image('city', 'assets/gotham.jpg');
-    game.load.image('floor', 'assets/floor.png');
-    game.load.image('glass', 'assets/glass.png');
-    game.load.image('weapon', 'assets/weapon.png');
-    game.load.spritesheet('detective', 'assets/player1.png', 31, 52);
-    game.load.spritesheet('robber', "./assets/player2.png", 31, 52);
-  } //preload
+function preload() {
+  game.load.image('city', 'assets/city.png');
+  game.load.image('floor', 'assets/floor.png');
+  game.load.image('glass', 'assets/glass.png');
+  game.load.image('weapon', 'assets/weapon.png');
+  game.load.spritesheet('detective', 'assets/player1.png', 31, 52);
+  game.load.spritesheet('robber', "./assets/player2.png", 31, 52);
+} //preload
 
 ///////////////////////////////////////////////////////
-//                     CREATE                        //
+//               CREATE - FUNCTION                   //
 ///////////////////////////////////////////////////////
 var platforms;
-var player1
+var ledge;
+var ground;
+var player1;
+var player2;
+var timer;
+var score1 = 0;
+var score2 = 0;
+var scoreText1;
+var scoreText2;
+var timerText;
 function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    platforms = game.add.group();
-    platforms.enableBody = true;
+  game.add.sprite(0, -60, 'city');
 
-    var ground = platforms.create(0, game.world.height - 40, "floor");
-    ground.scale.setTo(2,2)
-    ground.body.immovable = true;
+  platforms = game.add.group();
+  platforms.enableBody = true;
 
-    ground = platforms.create(375, game.world.height - 40, "floor");
-    ground.scale.setTo(2,2)
-    ground.body.immovable = true;
+  ground = platforms.create(0, game.world.height - 40, "floor");
+  ground.scale.setTo(2,2)
+  ground.body.immovable = true;
 
-    ground = platforms.create(750, game.world.height - 40, "floor");
-    ground.scale.setTo(2,2)
-    ground.body.immovable = true;
+  ground = platforms.create(375, game.world.height - 40, "floor");
+  ground.scale.setTo(2,2)
+  ground.body.immovable = true;
 
-    ground = platforms.create(1125, game.world.height - 40, "floor");
-    ground.scale.setTo(2,2)
-    ground.body.immovable = true;
+  ground = platforms.create(750, game.world.height - 40, "floor");
+  ground.scale.setTo(2,2)
+  ground.body.immovable = true;
 
-    var ledge = platforms.create(Math.random()*320, 550, "floor");
-    ledge.body.immovable = true;
+  ground = platforms.create(1125, game.world.height - 40, "floor");
+  ground.scale.setTo(2,2)
+  ground.body.immovable = true;
 
-    ledge = platforms.create((Math.random()*320)+600, 550, "floor");
-    ledge.body.immovable = true;
+  // platform 1 (top)
+  ledge = platforms.create(Math.random()*320, 175, "floor");
+  ledge.body.immovable = true;
+  ledge = platforms.create((Math.random()*320)+ 600, 175, "floor");
+  ledge.body.immovable = true;
 
-    ledge = platforms.create(Math.random()*320, 425, "floor");
-    ledge.body.immovable = true;
+  // platform 2
+  ledge = platforms.create(Math.random()*900, 300, "floor");
+  ledge.body.immovable = true;
 
-    ledge = platforms.create((Math.random()*320)+600, 425, "floor");
-    ledge.body.immovable = true;
+  // platform 3
+  ledge = platforms.create(Math.random()*320, 425, "floor");
+  ledge.body.immovable = true;
+  ledge = platforms.create((Math.random()*320)+ 600, 425, "floor");
+  ledge.body.immovable = true;
 
-    ledge = platforms.create(Math.random()*320, 300, "floor");
-    ledge.body.immovable = true;
-
-    ledge = platforms.create((Math.random()*320)+600, 300, "floor");
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(Math.random()*320, 175, "floor");
-    ledge.body.immovable = true;
-
-    ledge = platforms.create((Math.random()*320)+ 600, 175, "floor");
-    ledge.body.immovable = true;
+  // platform 4 (bottom)
+  ledge = platforms.create(Math.random()*900, 550, "floor");
+  ledge.body.immovable = true;
 
 
   ///////////////////////////////////////////////////////
   //                     PLAYER 1                      //
   ///////////////////////////////////////////////////////
-    player1 = game.add.sprite(32, game.world.height - 150, 'detective');
-    player1.scale.setTo(1, 1);
-    game.physics.arcade.enable(player1);
-    player1.body.bounce.y = 0.2;
-    player1.body.gravity.y = 300;
-    player1.body.collideWorldBounds = true;
-    player1.animations.add('right', [0,1,2,3], 10, true);
-    player1.animations.add('left', [5,6,7,8], 10, true);
+  player1 = game.add.sprite(32, game.world.height - 150, 'detective');
+  player1.scale.setTo(1, 1);
+  game.physics.arcade.enable(player1);
+  player1.body.bounce.y = 0.2;
+  player1.body.gravity.y = 300;
+  player1.body.collideWorldBounds = true;
+  player1.animations.add('right', [0,1,2,3], 10, true);
+  player1.animations.add('left', [5,6,7,8], 10, true);
 
   ///////////////////////////////////////////////////////
   //                     PLAYER 2                      //
   ///////////////////////////////////////////////////////
-    player2 = game.add.sprite(game.world.width - 64, game.world.height - 150, 'robber');
-    player2.scale.setTo(1, 1);
-    game.physics.arcade.enable(player2);
-    player2.body.bounce.y = 0.2;
-    player2.body.gravity.y = 300;
-    player2.body.collideWorldBounds = true;
-    player2.animations.add('right', [0,1,2,3], 10, true);
-    player2.animations.add('left', [5,6,7,8], 10, true);
-
-
+  player2 = game.add.sprite(game.world.width - 64, game.world.height - 150, 'robber');
+  player2.scale.setTo(1, 1);
+  game.physics.arcade.enable(player2);
+  player2.body.bounce.y = 0.2;
+  player2.body.gravity.y = 300;
+  player2.body.collideWorldBounds = true;
+  player2.animations.add('right', [0,1,2,3], 10, true);
+  player2.animations.add('left', [5,6,7,8], 10, true);
 
   ///////////////////////////////////////////////////////
   //                     OBJECTS                       //
   ///////////////////////////////////////////////////////
-    glasses = game.add.group()
-    glasses.enableBody = true;
+  glasses = game.add.group()
+  glasses.enableBody = true;
 
 
-    for (var i = 0; i < 10; i++) {
-      var glass = glasses.create( i * 120 , Math.random()* 500, 'glass')
-      glass.body.gravity.y = 125;
-      glass.body.bounce.y = 0.4 + Math.random()*0.2;
-    }
+  for (var i = 0; i < 10; i++) {
+    var glass = glasses.create( i * 120 , Math.random()* 500, 'glass')
+    glass.body.gravity.y = 125;
+    glass.body.bounce.y = 0.4 + Math.random()*0.2;
+  }
 
-    weapons = game.add.group()
-    weapons.enableBody = true;
+  weapons = game.add.group()
+  weapons.enableBody = true;
 
-    for (var i = 0; i < 10; i++) {
-        var weapon = weapons.create( i * 130 , Math.random()* 500, 'weapon')
-        weapon.body.gravity.y = 125;
-        weapon.body.bounce.y = 0.4 + Math.random()*0.2;
-    }
+  for (var i = 0; i < 10; i++) {
+    var weapon = weapons.create( i * 130 , Math.random()* 500, 'weapon')
+    weapon.body.gravity.y = 125;
+    weapon.body.bounce.y = 0.4 + Math.random()*0.2;
+  }
+
+  scoreText1 = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  
+  scoreText2 = game.add.text(game.world.width - 150, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  
+  TimerText = game.add.text(550, 16, 'Timer: 30', {fill: '#000'});
+
 } //create
 
 ///////////////////////////////////////////////////////
-//                     UPDATE                        //
+//               UPDATE - FUNCTION                   //
 ///////////////////////////////////////////////////////
 function update() {
   cursors = game.input.keyboard.createCursorKeys();
-  // wasd = {
-  //   up: XV.game.input.keyboard.addKey(Phaser.Keyboard.W)
-  //   down: XV.game.input.keyboard.addKey(Phaser.Keyboard.S)
-  //   left: XV.game.input.keyboard.addKey(Phaser.Keyboard.A)
-  //   right: XV.game.input.keyboard.addKey(Phaser.Keyboard.D)
-  // };
   game.physics.arcade.collide(player1, platforms);
   game.physics.arcade.collide(player2, platforms);
   game.physics.arcade.collide(player1, player2);
   game.physics.arcade.collide(weapons, platforms);
   game.physics.arcade.collide(glasses, platforms);
-  game.physics.arcade.overlap(player1, glasses, collectGlass, null, this);
+  game.physics.arcade.overlap(player1, weapons, collectWeapon, null, this);
+  game.physics.arcade.overlap(player2, glasses, collectGlass, null, this);
 
 
 
@@ -153,7 +155,7 @@ function update() {
         player1.frame = 4;
     }
     if (game.input.keyboard.addKey(Phaser.Keyboard.W).isDown && player1.body.touching.down) {
-        player1.body.velocity.y = -150;
+        player1.body.velocity.y = -350;
     }
     if (game.input.keyboard.addKey(Phaser.Keyboard.S).isDown) {
       player1.body.velocity.y = 200;
@@ -181,16 +183,65 @@ function update() {
     if (cursors.down.isDown) {
       player2.body.velocity.y = 250;
     }
-
-
 } //update
 
 ///////////////////////////////////////////////////////
-//        DETECTIVE COLLECTS MAGNIFYING GLASS       //
+//                  COLLECT OBJECTS                  //
 ///////////////////////////////////////////////////////
-function collectGlass(player1, glass) {
-  glass.kill();
-} // collectGlass
-function collectWeapons(player1, weapon) {
+function collectWeapon(player1, weapon) {
   weapon.kill();
+  score += 10;
+  scoreText1.text = 'Score: ' + score;
+  scoring1()
+} // collectWeapon
+function collectGlass(player2, glass) {
+  glass.kill();
+  score += 10;
+  scoreText2.text = 'Score: ' + score;
+  scoring2();
+} // collectGlass
+
+function scoring1() {
+  if (score1 == 50) {
+    clearTimer();
+    restart = true;
+  }
 }
+
+var timer = 30;
+
+var myInterval = setInterval(function() {
+  timer --
+  console.log(timer)
+  TimerText.text = 'Timer ' + timer
+  if (timer == 0) {
+    // alert("you lose")
+    gameOver = true;
+    clearTimer();
+    console.log(gameOver)
+    // location.reload()
+    document.getElementById('game').innerHTML = ''
+    gameEnd();
+  }
+},1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
